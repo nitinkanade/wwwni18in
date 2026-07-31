fbfa0ce6da29941922cf39f60807f192a976e549
Nitin Kanade <nitinkanade027@gmail.com>
2026-08-01 04:13:22 +0530

    Add offline PWA Service Worker caching and complete Google BreadcrumbList schema

---
+++ public/sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'ni18-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/wwwni18in/',
+  '/wwwni18in/favicon.svg',
+  '/wwwni18in/images/logo.svg',
+  '/wwwni18in/manifest.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        fetch(event.request).then((networkResponse) => {
+          if (networkResponse && networkResponse.status === 200) {
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, networkResponse));
+          }
+        }).catch(() => {});
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
