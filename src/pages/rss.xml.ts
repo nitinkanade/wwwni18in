0cce6aeaf6debb73b28c3e57e5098440f3bba365
Nitin Kanade <nitinkanade027@gmail.com>
2026-08-01 03:36:40 +0530

    Optimize SEO: update robots.txt sitemaps, RSS feed links, and BlogPosting schema

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,19 +1,22 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import { getUrl } from '../utils/navigation';
 
 export async function GET(context: any) {
   const posts = await getCollection('blog', ({ data }) => !data.draft);
   posts.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
 
   return rss({
-    title: 'NI18 Engineering & Tech Blog',
-    description: 'Technical articles, Android Jetpack Compose tutorials, SFCC e-commerce guides, and Web performance insights from NI18.',
+    title: 'NI18 Mobile Apps Articles & Guides',
+    description: 'Exam revision sheets, formula guides, MPSC tips, and English grammar lessons from NI18 Android Applications.',
     site: context.site,
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/blog/${post.slug}/`,
+      link: post.data.appId 
+        ? getUrl(`/apps/${post.data.appId}/blog/${post.slug}`) 
+        : getUrl(`/blog/${post.slug}`),
       author: post.data.author
     })),
     customData: `<language>en-us</language>`
